651c19f2eadb0303e7f3adc836a9e5d77261961f
luisco <luis.castellanos.oviedo@gmail.com>
2017-04-17 16:18:19 -0500

    First Commit java 8

---
+++ LearningJava8/src/test/java/test/TestOne.java
@@ -0,0 +1,17 @@
+package test;
+
+//import org.junit.Test;
+
+
+import org.junit.Test;
+
+/**
+ * Created by luis on 4/17/2017.
+ */
+public class TestOne {
+
+    @Test
+    public void testOne(){
+    }
+
+}
